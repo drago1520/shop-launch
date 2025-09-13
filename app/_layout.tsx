c6100b02a635ae4b562f7cbf00553e27196548fb
dragomir1520 <dragomir1520@gmail.com>
2025-09-13 20:32:48 +0300

    feat/analytics: posthog

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,23 +3,39 @@ import '@/global.css';
 import { NAV_THEME } from '@/lib/theme';
 import { ThemeProvider } from '@react-navigation/native';
 import { PortalHost } from '@rn-primitives/portal';
-import { Stack } from 'expo-router';
+import { Stack, useGlobalSearchParams, usePathname } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import { useColorScheme } from 'nativewind';
+import { PostHogProvider, usePostHog } from 'posthog-react-native';
+import { useEffect } from 'react';
 
-export {
-  // Catch any errors thrown by the Layout component.
-  ErrorBoundary,
-} from 'expo-router';
+export { ErrorBoundary } from 'expo-router';
 
 export default function RootLayout() {
   const { colorScheme } = useColorScheme();
+  const pathname = usePathname();
+  const params = useGlobalSearchParams();
+  const posthog = usePostHog(); // use the usePostHog hook if using the PostHogProvider or your own custom posthog instance
+  // Track the location in your analytics provider here.
+  useEffect(() => {
+    posthog.screen(pathname, params);
+  }, [pathname, params, posthog]);
 
   return (
-    <ThemeProvider value={NAV_THEME[colorScheme ?? 'light']}>
-      <StatusBar style={colorScheme === 'dark' ? 'light' : 'dark'} />
-      <Stack />
-      <PortalHost />
-    </ThemeProvider>
+    <PostHogProvider
+      apiKey="phc_rtWVkurqJiBqLjZxalfmh2oppe3kwtShfrC9BNqEmhP"
+      options={{
+        host: 'https://eu.i.posthog.com',
+        enableSessionReplay: true,
+        captureAppLifecycleEvents: true,
+      }}
+      autocapture={true}
+    >
+      <ThemeProvider value={NAV_THEME[colorScheme ?? 'light']}>
+        <StatusBar style={colorScheme === 'dark' ? 'light' : 'dark'} />
+        <Stack />
+        <PortalHost />
+      </ThemeProvider>
+    </PostHogProvider>
   );
 }
