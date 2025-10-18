de7b50d5944a9602c0cc5b5dcc09c28987a6ee47
drago1520 <141066422+drago1520@users.noreply.github.com>
2025-10-18 20:47:40 +0300

    feat/server: hello world for testing ci

--- server/client-trpc.ts
+++ server/client-trpc.ts
@@ -12,7 +12,7 @@ export const API_ = createTRPCClient<AppRouter>({
 export const BunTRPC = createTRPCClient<AppRouter>({
   links: [
     httpBatchLink({
-      url: process.env.EXPO_PUBLIC_BASE_URL_SERVER!,
+      url: `${process.env.EXPO_PUBLIC_BASE_URL_SERVER!}/trpc`,
     }),
   ],
 });
--- server/perf/bun-trpc.ts
+++ server/perf/bun-trpc.ts
@@ -3,10 +3,19 @@ import { createBunServeHandler } from 'trpc-bun-adapter';
 
 // Minimal Bun + tRPC handler wiring. Using `appRouter` exported from `server/index.ts`.
 Bun.serve(
-  createBunServeHandler({
-    router: appRouter,
-    createContext: () => ({}),
-  }),
+  createBunServeHandler(
+    {
+      router: appRouter,
+      endpoint: '/trpc',
+      createContext: () => ({}),
+    },
+    {
+      fetch(request, server) {
+        // will be executed if it's not a TRPC request
+        return new Response('Hello world');
+      },
+    },
+  ),
 );
 
 console.log('Server running at http://localhost:3000');
