8bf605d5abb16d33a3b0a7a1c58263fe17a65e32
drago1520 <141066422+drago1520@users.noreply.github.com>
2025-10-19 00:31:36 +0300

    test coolify deploy

---
+++ server/perf/index.ts
@@ -0,0 +1,12 @@
+const PORT = process.env.PORT || 3000;
+Bun.serve({
+  port: PORT,
+  fetch(req) {
+    const url = new URL(req.url);
+    if (url.pathname === '/') return new Response(`Home page! Bun version: ${Bun.version}`);
+    if (url.pathname === '/blog') return new Response('Blog!');
+    if (url.pathname === '/about') return new Response('About!');
+    return new Response('404!');
+  },
+});
+console.log(`Server running at http://localhost:${PORT}`);
